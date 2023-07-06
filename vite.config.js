8db1b293da1297e0fadc3c2235abe0b20598e612
Borislav <borislav.bondarev@gmail.com>
2023-07-06 18:11:21 +0300

    small change

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve:{
-    alias:{
-      '@': path.resolve("/public",)
-    },
-  },
   plugins: [react()],
   base: '/shri2023-task/',
 })
