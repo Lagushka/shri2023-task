1899856c079cbb5976c234ef58d5e83d97c887a3
Borislav <borislav.bondarev@gmail.com>
2023-07-09 20:49:14 +0300

    some optimization

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import Main from './components/Main';
 import { Header } from './components/Header';
-import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/shri2023-task/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html")
+      }
+    }
+  },
+  esbuild: {
+    minify: "esbuild"
+  }
 })
